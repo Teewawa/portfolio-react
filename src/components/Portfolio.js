05fd21d049ac65b975a2013cbb86f3a560b74a28
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-22 16:51:54 -0500

    Created Portfolio component
    To contain sections/components of the portfolio

---
+++ src/components/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/Portfolio.css";
+
+export default function Portfolio() {
+  return (
+    <div className="Portfolio">
+      <h3>hello from portfolio</h3>
+    </div>
+  );
+}
