6ae9d2ace8d9952c1c4163f4ac1b0d889bc9a136
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-25 18:55:29 -0500

    Created Certificates component

---
+++ src/components/Certificates.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/Certificates.css";
+
+export default function Certificates() {
+  return (
+    <div className="Certificates">
+      this will be a gallery displaying certificates, one main large display,
+      smaller ones below. When click, displays in main large
+    </div>
+  );
+}
