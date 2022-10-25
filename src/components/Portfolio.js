020f078cc05cae14882b7da5f6c2594392f41bcb
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-25 18:56:02 -0500

    Moved featured content into Featured component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/Portfolio.css";
-import dictionary from "../assets/dictionary-mockup2.jpg";
-import travelGU from "../assets/travel-guam-mockup.jpg";
+import Featured from "./Featured";
+import Certificates from "./Certificates";
 
 export default function Portfolio() {
   return (
@@ -9,27 +9,9 @@ export default function Portfolio() {
       <main>
         <h3>FEATURED PROJECTS</h3>
         <p>based off my most recent work </p>
-        <section className="grid grid-2col">
-          <section>
-            <h4>Simple Dictionary</h4>
-            <p>Web Technologies: React, API</p>
-            <img
-              src={dictionary}
-              alt=""
-            />
-          </section>
-
-          <section>
-            <h4>Travel to Guam </h4>
-            <p>HTML, CSS, Advance Responsive Design</p>
-            <img
-              src={travelGU}
-              alt=""
-            />
-          </section>
-        </section>
-
+        <Featured />
         <a href="/">View All Projects</a>
+        <Certificates />
       </main>
     </div>
   );
