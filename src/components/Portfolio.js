3bd4abb1e3bb07e5bda77b0bcef6cc571e517cb4
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-25 17:48:43 -0500

    Added main and section elements
    Included classes to create grid for featured projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,30 +1,36 @@
 import React from "react";
 import "../css/Portfolio.css";
-import dictionary from "../assets/dictionary-mockup.jpg";
+import dictionary from "../assets/dictionary-mockup2.jpg";
 import travelGU from "../assets/travel-guam-mockup.jpg";
 
 export default function Portfolio() {
   return (
     <div className="Portfolio">
-      <h3>FEATURED PROJECTS</h3>
-      <p>based off my most recent work </p>
-      <section>
-        <h4>Simple Dictionary</h4>
-        <p>Web Technologies: React, API</p>
-        <img
-          src={dictionary}
-          alt=""
-        />
-      </section>
-      <section>
-        <h4>Travel to Guam Landing Page</h4>
-        <p>HTML, CSS, Advance Responsive Design</p>
-        <img
-          src={travelGU}
-          alt=""
-        />
-      </section>
-      <a href="/">View All Projects</a>
+      <main>
+        <h3>FEATURED PROJECTS</h3>
+        <p>based off my most recent work </p>
+        <section className="grid grid-2col">
+          <section>
+            <h4>Simple Dictionary</h4>
+            <p>Web Technologies: React, API</p>
+            <img
+              src={dictionary}
+              alt=""
+            />
+          </section>
+
+          <section>
+            <h4>Travel to Guam </h4>
+            <p>HTML, CSS, Advance Responsive Design</p>
+            <img
+              src={travelGU}
+              alt=""
+            />
+          </section>
+        </section>
+
+        <a href="/">View All Projects</a>
+      </main>
     </div>
   );
 }
