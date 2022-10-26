9dd90c9588c40e4834e33e72d5b68cdc72236db8
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-26 17:23:39 -0500

    Created Experience.js component

---
+++ src/components/Experience.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Experience() {
+  return (
+    <div
+      className="Experience"
+      id="experience"
+    >
+      "this is the work experience section"
+    </div>
+  );
+}
