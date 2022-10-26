5d0a5451caa3658a09fc2bbb135f5a377fb256c0
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-26 17:24:27 -0500

    Updated content within featured section

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -5,29 +5,34 @@ import travelGU from "../assets/travel-guam-mockup.jpg";
 
 export default function Featured() {
   return (
-    <div className="Featured">
+    <div
+      className="Featured"
+      id="featured"
+    >
+      <h3>FEATURED PROJECTS</h3>
+
       <section className="grid grid-2col">
-        <section>
-          <h4>Simple Dictionary</h4>
-          <p>Web Technologies: React, API</p>
+        <div className="featured-project">
+          <h4>simple dictionary</h4>
+          <p>REACT | API</p>
           <img
             src={dictionary}
             alt=""
           />
           <a href="/">Launch Project</a>
-        </section>
+        </div>
 
-        <section>
-          <h4>Travel to Guam </h4>
-          <p>HTML, CSS, Advance Responsive Design</p>
-
+        <div className="featured-project">
+          <h4>travel to guam </h4>
+          <p>HTML | PURE CSS | RESPONSIVE</p>
           <img
             src={travelGU}
             alt=""
           />
           <a href="/">Launch Project</a>
-        </section>
+        </div>
       </section>
+      <a href="/">View All Projects</a>
     </div>
   );
 }
