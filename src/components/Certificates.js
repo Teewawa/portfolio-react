e2028cb500eeac23e53239b2e892813922694709
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-25 23:31:35 -0500

    Creating custom image gallery for certs
    Displays one large certificate, with smaller sized certificates below (organized in a grid). When clicked, the large certificate will update to specific certificate that was clicked on

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Certificates.css";
+import IntroToCode from "../assets/Introduction to Coding SheCodes Cert - Tiara Mendiola Apr 2022.png";
+import WebDevelopment from "../assets/Web Development SheCodes Cert - Tiara Mendiola.png";
+import Responsive from "../assets/Responsive Web Development SheCodes Cert - Tiara Mendiola.png";
 
 export default function Certificates() {
+  let [certificateImage, setCertificateImage] = useState(IntroToCode);
+  const certificateList = [IntroToCode, WebDevelopment, Responsive];
+
   return (
     <div className="Certificates">
-      this will be a gallery displaying certificates, one main large display,
-      smaller ones below. When click, displays in main large
+      <img
+        src={certificateImage}
+        alt=""
+        className="certificate-lg"
+      />
+      <div className="grid grid-certificate-sm">
+        {certificateList.map(function (certificate, index) {
+          return (
+            <div key={index}>
+              <a href="/">
+                <img
+                  src={certificate}
+                  alt=""
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCertificateImage(certificate);
+                  }}
+                />
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
