b33eb6426a9d708a4045e48a3dc4220eebf16a6c
Tiara Mendiola <dev.tiaram@gmail.com>
2022-11-01 15:45:44 -0500

    Added list of sections to add details to
    needs work - currently a list of ideas to add for this section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,23 @@ export default function Experience() {
       className="Experience"
       id="experience"
     >
-      "this is the work experience section"
+      <h3>Experience</h3>
+      <h3>Front End Development & Web Technologies </h3>
+      <p>Skills here</p>
+      <p>noteable projects</p>
+      <p>View all projects</p>
+      <p>Dictionary Application</p>
+      <p>Travel Landing Page</p>
+      <p>Weather Application</p>
+
+      <h3>Employment History</h3>
+      <p>Junior Computer Operator</p>
+      <p> accelerated CCA training by 50% - Terminal/FTP -</p>
+      <p>Computer Center Assistant:</p>
+      <p>
+        Troubleshooting hardware & software, maintenance & repair, data center
+        floor blueprint
+      </p>
     </div>
   );
 }
