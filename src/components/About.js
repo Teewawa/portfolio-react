9c1e594944371d64a9354ee708c90c841fdc91e1
Tiara Mendiola <dev.tiaram@gmail.com>
2022-11-01 15:44:36 -0500

    Created About.js
    Mini bio description

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/About.css";
+
+export default function About() {
+  return (
+    <div className="About">
+      <p>Hello world!</p>
+      <p>
+        I'm on a top secret mission. I can't tell you what that is exactly, but
+        I decided to take the scenic route. I grew up in the early 90s, meaning
+        I saw & experienced the evolution of today's technology. It was only a
+        matter of time before I graduated with a BA in Computer Science then
+        climbed ladders, ran lines, and repaired computers with a small IT Team,
+        and now I write things that will turn into 1's and 0's (machine
+        language). When I'm not entering the matrix, you'll probably find me
+        cooking something I've never made before, watching movies, anime,
+        history & myths, or playing an old video game with considerations to
+        stream it live. I also enjoy learning anything because it keeps the
+        sparkle in my eyes & the fire alive.
+      </p>
+    </div>
+  );
+}
