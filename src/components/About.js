34df21cccff9e358ad226364e9e9f6908af8cbcd
Tiara Mendiola <dev.tiaram@gmail.com>
2022-11-02 15:29:41 -0500

    Added a wave divider, updated bio
    wave divider from shapedivider.app

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,56 @@
 import React from "react";
 import "../css/About.css";
+import gameOfTech from "../assets/TiaraMendiola-GameOfTech.jpg";
+import profilePicture from "../assets/TiaraMendiola-Profile-.jpg";
+import profileHoodie from "../assets/TiaraMendiola-Profile2.jpg";
 
 export default function About() {
   return (
     <div className="About">
-      <p>Hello world!</p>
-      <p>
-        I'm on a top secret mission. I can't tell you what that is exactly, but
-        I decided to take the scenic route. I grew up in the early 90s, meaning
-        I saw & experienced the evolution of today's technology. It was only a
-        matter of time before I graduated with a BA in Computer Science then
-        climbed ladders, ran lines, and repaired computers with a small IT Team,
-        and now I write things that will turn into 1's and 0's (machine
-        language). When I'm not entering the matrix, you'll probably find me
-        cooking something I've never made before, watching movies, anime,
-        history & myths, or playing an old video game with considerations to
-        stream it live. I also enjoy learning anything because it keeps the
-        sparkle in my eyes & the fire alive.
-      </p>
+      <section>
+        <div className="wave">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+      </section>
+
+      <section className="grid grid-2col">
+        <div>
+          <h5>HELLO WORLD</h5>
+          <h6>I'M ON A SECRET MISSION</h6>
+          <h6>I CAN'T TELL YOU, BUT I DECIDED </h6>
+          <h6>TO TAKE THE SCENIC ROUTE</h6>
+          <p>
+            I grew up in the early 90s, meaning I saw & experienced the
+            evolution of today's technology. So it was only a matter of time
+            before I graduated with a Bachelors in Computer Science and started
+            climbing ladders, running lines, and repairing computers with a
+            small IT Team. Now I write words that will turn into 1's and 0's
+            (machine language). When I'm not entering the matrix, you'll
+            probably find me cooking something I've never made before, watching
+            movies, anime, history & myths, or playing an old video game with
+            considerations on live streaming. I love learning, building, and
+            trying new things because it keeps the sparkle in my eyes & the fire
+            alive.
+          </p>
+        </div>
+
+        <div class>
+          <img
+            src={gameOfTech}
+            alt=""
+          />
+        </div>
+      </section>
     </div>
   );
 }
